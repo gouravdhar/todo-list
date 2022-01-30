3a070f15cd343ed1b17fc9c233a9ffcc88379ca4
batman <dharg57@gmail.com>
2022-01-30 17:12:19 +0530

    first commit

---
+++ precache-manifest.6fdc2ceced6ad159109f1b030c263280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f912d91a6d7bd026415ed7e139c62a8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f124a17f3a2838917687",
+    "url": "/static/css/main.ef8b9f35.chunk.css"
+  },
+  {
+    "revision": "96f377e1e66c31bfba71",
+    "url": "/static/js/2.e552bf3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e552bf3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f124a17f3a2838917687",
+    "url": "/static/js/main.af8dfdb3.chunk.js"
+  },
+  {
+    "revision": "e5bd49b5e44f37480bf2",
+    "url": "/static/js/runtime-main.25e9298a.js"
+  }
+]);
